f4c1b1767b4cf47efd08cee618c05d200c6a1ed8
Louis Labes <labeslouis@gmail.com>
2021-08-18 15:06:19 +0200

    LeFuck

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,7 @@ class Trip < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_one_attached :photo
+
+  geocoded_by :destination
+  after_validation :geocode, if: :will_save_change_to_destination?
 end
